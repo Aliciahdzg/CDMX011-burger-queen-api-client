b069e79572fb3faec5c26892926ea592009379a4
Aliciahdzg <hernndez.alicia4@gmail.com>
2021-11-17 00:41:22 -0600

    diseño del menu Ãpara agregar los items al resumen de pedidos

--- burgerqueen/src/components/Resum.jsx
+++ burgerqueen/src/components/Resum.jsx
@@ -1,24 +1,45 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Resum = (props) => {
   const { resumItems, removeAll } = props;
+  const [total, setTotal] = useState([]);
+
+  useEffect(() => {
+    if (resumItems.length === 0) {
+      return 0;
+    }
+    const prices = resumItems.map((item) => item.qty * item.price);
+    const reducer = (previousValue, currentValue) =>
+      previousValue + currentValue;
+    return setTotal(prices.reduce(reducer));
+  }, [resumItems]);
+
   return (
     <div>
-      <h3>Resumen del pedido</h3>
-      <div>{resumItems.length === 0 && <p>El pedido esta vacio</p>}</div>
-      {resumItems.map((item) => (
-        <div key={item.id}>
-          <ul>
-            <li>{item.name}</li>
-            <li> cant {item.qty}</li>
-            <li>total $ {item.qty * item.price}</li>
-          </ul>
-          <button type="button" onClick={() => removeAll(item)}>
-            Borrar
-          </button>
-        </div>
-      ))}
+      <div className="div-client-name">
+        <input className="client-nane" placeholder="Nombre del cliente" />
+      </div>
+      <div className="resum-content">
+        <h3> Resumen del pedido</h3>
+        <div>{resumItems.length === 0 && <p>El pedido esta vacio</p>}</div>
+        {resumItems.map((item) => (
+          <div key={item.id}>
+            <ul>
+              <li>{item.name}</li>
+              <li> cant {item.qty}</li>
+              <li>total $ {item.qty * item.price}</li>
+            </ul>
+            <button type="button" onClick={() => removeAll(item)}>
+              Borrar
+            </button>
+          </div>
+        ))}
+      </div>
+      <div>
+        <p>Total: ${total}</p>
+        <button type="button">Enviar</button>
+      </div>
     </div>
   );
 };
